1d2297e554fe082e585ff7fc4475447ae34b15d4
roncwynn <rcwynn@gmail.com>
2020-05-21 09:19:47 -0400

    First round of tests added to new and existing classes

---
+++ VirtualPet/Shelter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPet
+{
+    public class Shelter
+    {
+        const int ShelterCapacity = 10;
+
+        public int totalShelterPetCount { get; set; }
+
+        public Shelter()
+        {
+
+        }
+
+        public bool IsShelterFull()
+        {
+            throw new NotImplementedException();
+        }
+        public int GetShelterPetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddPetToShelter(Pet somePet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemovePetFromShelter(Pet somePet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetShelterCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+    
+}
